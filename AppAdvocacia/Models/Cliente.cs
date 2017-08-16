42c7e3d69c20a2cdeabf926ed83e17237ac3ff48
mateusgadotti <mateus.gadotti@catolicasc.org.br>
2017-08-15 21:41:19 -0300

    modificado o layout

--- AppAdvocacia/Models/Cliente.cs
+++ AppAdvocacia/Models/Cliente.cs
@@ -10,7 +10,15 @@ namespace AppAdvocacia.Models
 
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string Nome { get; set; }
+
+         public string Sobrenome { get; set; }
+
+        public string CPF { get; set; }
+
+        public string Endereco { get; set; }
+
+
 
     }
 }
